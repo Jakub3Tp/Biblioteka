125b64d28a1531af5661b8441d9f799ae66bcf5c
Przemek B <pprzemek89@gmail.com>
2024-02-19 10:27:43 +0100

    Budujemy dalej :D

--- src/Biblioteka.java
+++ src/Biblioteka.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class Biblioteka {
+    ArrayList<Ksiazka> ksiazki = new ArrayList<>();
+    ArrayList<Egzemplarz> egzemplarze = new ArrayList<>();
+    ArrayList<Wypozyczenie> wypozyczenia = new ArrayList<>();
+
+    public void dodajKsiazke(){
+        // TODO dodanie nowego egzemplarza ksiazki. Jesli ksiazki juz istnieje wez referencje w przeciwnym
+        // TODO przypadku stworz nowa ksiazke
+    }
+
+    public Ksiazka znajdzKsiazke (String tytul){
+        return null;
+    }
+
+    public ArrayList<Ksiazka> znajdzKsiazkiAutora(Autor autor){
+        for (Ksiazka k: ksiazki){
+           if (autor.equals(Egzemplarz.Stan.Wolny)){
+
+           }
+        }
+        return null;
+    }
+
+    public ArrayList<Ksiazka> znajdzKsiazkiPoGatunku(Gatunek gatunek){
+        for (Ksiazka k: ksiazki){
+
+        }
+        return null;
+    }
+    public Egzemplarz znajdzEgzemplarzKsiazki(Ksiazka ksiazka){
+        for (Egzemplarz e: egzemplarze){
+            if(Egzemplarz.Stan.Wolny){
+
+            }
+        }
+        return null;
+    }
 }
